04445a686739db7523b15dd5c1633d37a4304866
Eliyaz Syed <esyed@buchanan.com>
2023-04-19 10:50:04 +0530

    image push

--- src/main/java/com/example/springboot/controller/SpringController.java
+++ src/main/java/com/example/springboot/controller/SpringController.java
@@ -23,7 +23,7 @@ public class SpringController {
 //		Integer sec = date.getSecond();
 		val++;
 //		Integer count = year + month + day + hour + min + sec + 1;
-		String result = "Welcome to Buchanan Technologies" + val;
+		String result = "Welcome to Buchanan Technologies, This is A Sample DEVOPS ProJect" + val;
 		return result;
 	}
 }
